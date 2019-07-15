2e7135f00f48b33f503f32491cb6e6d0b201ddbe
Tan-Jerry <tp8810@126.com>
2019-07-15 17:02:24 +0800

    ajax

---
+++ 02/scripts/getNewContent.js
@@ -0,0 +1,24 @@
+function getNewContent()
+{
+    let request = getHTTPObject();
+    if (request)
+    {
+        request.open("GET", "ajax_test.txt", true);
+        request.onreadystatechange = function(){
+            if (request.readyState == 4)
+            {
+                let para = document.createElement("p");
+                let txt = document.createTextNode(request.responseText);
+                para.appendChild(txt);
+
+                document.getElementById("new").appendChild(para);
+            }
+        }
+        request.send(null);
+    }
+    else
+    {
+        alert("Sorry, your browser doesn't support XMLHttpRequest");
+    }
+}
+addLoadEvent(getNewContent);
--- 02/scripts/showPic.js
+++ 02/scripts/showPic.js
@@ -1,3 +1,33 @@
+function addLoadEvent(func)
+{
+    let oldonload = window.onload;
+
+    if (typeof window.onload != "function")
+    {
+        window.onload = func;
+    }
+    else
+    {
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement, targetElement)
+{
+    let parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement)
+    {
+        parent.appendChild(newElement);
+    }
+    else
+    {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
 function showPic(whichPic)
 {
     var source = whichPic.getAttribute("href");
@@ -7,12 +37,46 @@ function showPic(whichPic)
     var title = whichPic.getAttribute("title");
     var description = document.getElementById("description");
     description.firstChild.nodeValue = title;
+
+    return false;
 }
 
 function countBodyChildren()
 {
     var body_element = document.getElementsByTagName("body")[0];
-    alert(body_element.childNodes.length);
+    alert(body_element.childNodes.length);  
 }
 
-window.onload=countBodyChildren;
+function prepareGallery()
+{
+    let gallery = document.getElementById("imagegallery");
+    let links = gallery.getElementsByTagName("a");
+
+    for (let link of links)
+    {
+        link.onclick = function(){
+            return showPic(this);
+        }
+    }
+}
+
+function preparePlaceholder()
+{
+    let placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/DSC00062.JPG");
+    placeholder.setAttribute("alt", "my image gallery");
+
+    let description = document.createElement("p");
+    description.setAttribute("id", "description");
+    let desctext = document.createTextNode("choose an image");
+    description.appendChild(desctext);
+
+    let gallery = document.getElementById("imagegallery");
+
+    insertAfter(placeholder, gallery);
+    insertAfter(description, placeholder);
+}
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
